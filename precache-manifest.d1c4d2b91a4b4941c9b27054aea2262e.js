f42368969d37a7ab8af3984ab1a67a2cdac9f759
Sean Standen <seanstanden@AJM-MacBook-Pro.attlocal.net>
2020-06-29 13:42:57 -0700

    Updates

---
+++ precache-manifest.d1c4d2b91a4b4941c9b27054aea2262e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd1a5f245c5fd42d87f09841d9a1f29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7f01874291c1ca983a5",
+    "url": "/static/css/2.09d1976d.chunk.css"
+  },
+  {
+    "revision": "2f7bbed93852ca34bfa9",
+    "url": "/static/css/main.07f4878d.chunk.css"
+  },
+  {
+    "revision": "d7f01874291c1ca983a5",
+    "url": "/static/js/2.c23a5f4a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c23a5f4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f7bbed93852ca34bfa9",
+    "url": "/static/js/main.857f0d5a.chunk.js"
+  },
+  {
+    "revision": "f7acd5660dcc6e54833a",
+    "url": "/static/js/runtime-main.3d18f0fa.js"
+  },
+  {
+    "revision": "831e44ab1f4d92cdab3ef7e85092f5a7",
+    "url": "/static/media/AgentOrange.831e44ab.jpg"
+  },
+  {
+    "revision": "4bd1e64730004a2725b0df62006ba03c",
+    "url": "/static/media/PressureSensitive.4bd1e647.png"
+  },
+  {
+    "revision": "90165463da7b486391346a26487d7d48",
+    "url": "/static/media/RightSupremacy.90165463.jpg"
+  }
+]);
